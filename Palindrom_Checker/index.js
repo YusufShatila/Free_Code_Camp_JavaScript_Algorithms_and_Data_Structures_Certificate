a1ccfa084bd4c15e7898859a65430060c5560381
Yusuf <yusufshatila457@gmail.com>
2023-09-20 15:30:42 +0300

    Palindrome Checker Done

--- Palindrom_Checker/index.js
+++ Palindrom_Checker/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+function palindrome(str) {
+    console.log(str);
+    let noSpaceString = str.replace(/\s+/g, '');
+    console.log(noSpaceString);
+    let noAlphanumericString = str.replace(/[^a-z0-9]/gi, '');
+    console.log(noAlphanumericString);
+    let upperCaseString = noAlphanumericString.toUpperCase();
+    console.log(upperCaseString)
+    let reversedString = upperCaseString.split('').reverse().join('');
+    console.log(reversedString);
+    
+    if (reversedString === upperCaseString) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  const result = palindrome("five|\_/|four");
+  console.log(result);
+  
